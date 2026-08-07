c9472d001373fe07b563e459f3f917e46991ccac
9uiLe <kobappleple@gmail.com>
2026-08-08 08:06:00 +0900

    Bootstrap Nix development environment

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Reproducible tooling for StillWant iOS development";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              actionlint
+              gh
+              jq
+              swiftformat
+              xcbeautify
+              xcodegen
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt
+      );
+    };
+}
